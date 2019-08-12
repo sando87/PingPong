1f4eca8347f7240ef08721af3feb2bb747e834a7
sando87 <sando87@gmail.com>
2019-08-13 08:27:34 +0900

    woring UI

---
+++ Assets/Scripts/ItemDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = Screen.width * 0.2f;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, height);
+    }
+}
---
+++ Assets/Scripts/MyHorizontalLayoutGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyHorizontalLayoutGroup : HorizontalLayoutGroup
+{
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        RectTransform rectTrans = (RectTransform)transform;
+        float height = rectTrans.rect.height - padding.top - padding.bottom;
+        RectTransform[] childs = GetComponentsInChildren<RectTransform>();
+        for(int i = 1; i < childs.Length; ++i)
+        {
+            RectTransform trans = childs[i];
+            trans.sizeDelta = new Vector2(height, height);
+        }
+
+        
+    }
+}
